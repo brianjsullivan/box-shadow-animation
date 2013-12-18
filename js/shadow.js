5d93494bd4dc96e4ef2fce86717a1343d3f79959
Brian Sullivan <brian.sullivan@complex.com>
2013-12-18 15:27:42 -0500

    Changed default shadow blur css/js

--- js/shadow.js
+++ js/shadow.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 		$('#button').stop().animate({boxShadow:'4px 4px 3px 0 #9a9a9a', top:'24px', left:'30px'}, 50);
 	});
 	$('#button').on('mouseup', function(){
-		$('#button').stop().animate({boxShadow:'10px 10px 15px 0 #9a9a9a', top:'20px', left:'25px'}, 300);
+		$('#button').stop().animate({boxShadow:'10px 10px 10px 0 #9a9a9a', top:'20px', left:'25px'}, 300);
 	});
 });
